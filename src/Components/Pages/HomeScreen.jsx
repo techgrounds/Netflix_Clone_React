b5f32b0ec49e0642bfa40db051cac26bc052b169
manoninonam <manon.heijmans@gmail.com>
2021-09-10 11:41:14 +0200

    Added react-router navigation
    added navigation
    changed components name to be more scalable.

--- src/Components/Pages/HomeScreen.jsx
+++ src/Components/Pages/HomeScreen.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-// import Header from "../Layout/Header";
-// import Footer from "../Layout/Footer";
 import FeaturedSlider from "../Contents/FeaturedSlider";
 import CatSlider from "../Contents/CatSlider";
 import homeImage from "../../Assets/images/slider/netflix-home-banner-02.jpg";
@@ -9,10 +7,8 @@ import homeImage from "../../Assets/images/slider/netflix-home-banner-02.jpg";
 const Home = (props) => {
   return (
     <>
-      {/* <Header {...props} /> */}
       <FeaturedSlider image={homeImage} {...props} />
       <CatSlider {...props} />
-      {/* <Footer {...props} /> */}
     </>
   );
 };
