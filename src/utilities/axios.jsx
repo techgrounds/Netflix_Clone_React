18dd76b35859a97e8f776d0f411b626e77b6fa21
Ephrem B <ephrem44be@yahoo.com>
2021-09-21 14:41:17 +0200

    update

--- src/utilities/axios.jsx
+++ src/utilities/axios.jsx
@@ -3,6 +3,9 @@ import axios from "axios";
 // BASE OF THE MOVIE URL
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
+
+  // baseURL:
+  //   "`https://immense-garden-85870.herokuapp.com/api/v1/discovery/${order}/${video}/${page}/${year}/${genre}`",
 });
 
 export default instance;
