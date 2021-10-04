56ca14bffdf419f934196f45bedd3d85400fcfdf
Ryno <r_romario@msn.com>
2021-10-04 15:05:37 +0200

    Manually merged Ryno's changes into Joeri's version

--- src/Components/Contents/ModalContent.js
+++ src/Components/Contents/ModalContent.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Container, Row, Col, Image } from 'react-bootstrap';
 import ModalFilmCard from './ModalFilmCard';
 import Modal from './ModalContainer'
-import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { FaChevronDown, FaChevronUp, FaVolumeMute, FaRegThumbsDown, FaRegThumbsUp, FaPlus, FaPlay } from "react-icons/fa";
 
 export default function ModalContent(props) {
     const [toggleExpanded, setToggleExpanded] = useState(false);
@@ -47,21 +47,48 @@ export default function ModalContent(props) {
                 <Container fluid id="modal-container" >
 
                     <Container fluid>
-                        <Row>
-                            <Col className="px-0">
-                                <video controls autoPlay muted src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4" width="100%" height="auto">
+                    <Row xs={1} className="px-0">
+                        <Col className="video-container px-0">
+                            <video autoPlay muted src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4" width="100%" height="auto">
                                 </video>
                             </Col>
+                            <Col id="video-controls" className="bg-transparent">
+                            <div className="d-flex justify-content-between">
+                                <div>
+                                    <button className="d-inline-block btn bg-white py-2 px-3 mr-3" ><FaPlay className="mr-3" />Afspelen</button>
+                                    <button className="circle-button mr-1"><FaPlus color="white" /></button>
+                                    <button className="circle-button mr-1"><FaRegThumbsUp color="white" /></button>
+                                    <button className="circle-button mr-1"><FaRegThumbsDown color="white" /></button>
+                                </div>
+                                <div>
+                                    <button className="circle-button mr-5"><FaVolumeMute color="white" /></button>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row id="film-summary" className="px-5">
+                        <Col xs={8}>
+                            <span className="match-text">xx% match</span>
+                            <Image className="age-pic" src="https://www.kijkwijzer.nl/upload/pictogrammen/1_120_AL.png" />
+                            <span>{"release date"}</span>
+                            <span>{"film-duration"}</span>
+                            <p className="">Here comes the description of the movie.</p>
+                        </Col>
+                        <Col className="summary-details">
+                            <p><span>Cast: </span>{"namen cast"}</p>
+                            <p><span>Genres: </span>{"genres"}</p>
+                            <p><span>Deze titel is: </span>{"category?"}</p>
+                        </Col>
                         </Row>
                     </Container>
 
                     <Container fluid className="meer-zoals-dit-container">
-                        <Row>
+                    <Row className="px-5">
                             <Col>
                                 <h2>{movie.title}</h2>
                             </Col>
                         </Row>
-                        <Row xs={1} sm={2} md={3} lg={4}>
+                        <Row xs={1} sm={2} md={3} lg={4} className="d-flex justify-content-between px-5">
                             <ModalFilmCard />
                             <ModalFilmCard />
                             <ModalFilmCard />
@@ -84,8 +111,8 @@ export default function ModalContent(props) {
                         </Row>
                     </Container>
                     <Container fluid>
-                        <Row className="">
-                            <Col className="px-0">
+                    <Row className="px-5">
+                        <Col>
                                 {/* <hr /> */}
                                 <div className="my-hr" />
                                 <button className="expand-mzd-button" onClick={toggleButton}>
@@ -96,12 +123,12 @@ export default function ModalContent(props) {
                     </Container>
 
                     <Container fluid className="trailers-en-meer-container">
-                        <Row>
+                    <Row className="px-5">
                             <Col>
                                 <h2>Trailers en meer</h2>
                             </Col>
                         </Row>
-                        <Row xs={3}>
+                        <Row xs={3} className="px-5">
                             <Col>
                                 <iframe width="200" height="auto" src="https://www.youtube.com/embed/DGX9UogxXeA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                                 <p>{"Film title"} (clip)/(trailer)</p>
@@ -114,7 +141,7 @@ export default function ModalContent(props) {
                     </Container>
 
                     <Container fluid className="modal-content-footer">
-                        <Row xs={1}>
+                    <Row xs={1} className="px-5">
                             <Col>
                                 <h2>Over {"filmtitle"}</h2>
                             </Col>
@@ -125,7 +152,7 @@ export default function ModalContent(props) {
                                 <p><span>Deze titel is: </span>{"category?"}</p>
                             </Col>
                         </Row>
-                        <Row>
+                        <Row className="px-5">
                             <Col xs="auto">
                                 <span>Leeftijdsclassificatie:</span>
                             </Col>
--- src/Components/Contents/ModalFilmCard.js
+++ src/Components/Contents/ModalFilmCard.js
@@ -1,27 +1,38 @@
 import React from 'react';
-import { Col, Container, Row, Image } from 'react-bootstrap';
-import { FaRegPlayCircle, FaPlus } from 'react-icons/fa'
+import { Col, Container, Row, Image, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { FaRegPlayCircle, FaPlus, } from 'react-icons/fa'
 
 export default function ModalFilmCard() {
     return (
-        <Col className="modal-card-container">
-            <Container fluid>
+        <Col className="modal-film-container px-0">
+            <Container fluid className="film-card-container">
                 <Row>
-                    <Col xs={12}>
-                        <Image fluid src="http://images6.fanpop.com/image/photos/40000000/The-Finest-Hours-Banner-movie-trailers-40025062-1200-638.jpg" className="le-image" />
+                    <Col xs={12} className="px-0" >
+                        <Image fluid src="http://images6.fanpop.com/image/photos/40000000/The-Finest-Hours-Banner-movie-trailers-40025062-1200-638.jpg" className="mzd-image" />
                     </Col>
                     <Col xs={12}>
+                        {/* <OverlayTrigger key="top" placement="top" overlay={
+                            <Tooltip id="tooltip-top">
+                                Toevoegen aan Mijn Lijst
+                            </Tooltip>
+                        }> */}
                         <FaRegPlayCircle className="play-button" color="white" />
+                        {/* </OverlayTrigger> */}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={8}>
+                        <p className="match-text">xx% match</p>
+                        <Image className="age-pic" src="https://www.kijkwijzer.nl/upload/pictogrammen/1_120_AL.png" />
+                        <span>{"release date"}</span>
+                    </Col>
+                    <Col>
+                        <button className="circle-button">< FaPlus color="white" /></button>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
-                    <p className="match-text">xx% match</p>
-                    <Image className="age-pic" src="https://www.kijkwijzer.nl/upload/pictogrammen/1_120_AL.png" />
-                    <span>{"release date"}</span>
-                    </Col>
-                    <Col>
-                    < FaPlus className="add-to-wishlist" color="white" />
+                        <p>Here comes a description</p>
                     </Col>
                 </Row>
             </Container>
