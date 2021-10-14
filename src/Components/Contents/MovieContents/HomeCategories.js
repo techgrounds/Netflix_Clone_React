0e2e373ba674275e6bf91dbd6518ca651d8b5323
manoninonam <manon.heijmans@gmail.com>
2021-10-14 15:52:55 +0200

    Fixed issue with z-index

--- src/Components/Contents/MovieContents/HomeCategories.js
+++ src/Components/Contents/MovieContents/HomeCategories.js
@@ -1,16 +1,16 @@
 import React from "react";
 import movieDataRequests from "../../../utilities/config";
 import CatSlider from "./CatSlider";
-import DemoEphrem from "../DemoEphrem";
+// import DemoEphrem from "../DemoEphrem";
 export default function HomeCategories(props) {
     return (
         <>
 
-            <DemoEphrem
+            {/* <DemoEphrem
                 title="Zeroes"
                 fetchUrl={movieDataRequests.fetchZeroes}
                 isLargeRow
-            />
+            /> */}
 
 
             <CatSlider
@@ -32,6 +32,18 @@ export default function HomeCategories(props) {
                 fetchUrl={movieDataRequests.fetchEighties}
             // isLargeRow
             />
+             <CatSlider
+                props={props}
+                title="Disney"
+                fetchUrl={movieDataRequests.fetchDisney}
+            // isLargeRow
+            />
+             {/* <CatSlider
+                props={props}
+                title="Ryan Reynolds"
+                fetchUrl={movieDataRequests.fetchActor}
+            // isLargeRow
+            /> */}
 
 
 
--- src/utilities/config.jsx
+++ src/utilities/config.jsx
@@ -3,8 +3,8 @@ const movieDataRequests = {
   fetchZeroes: `discovery/desc/true/1/2000/Action`,
   fetchNineties: `discovery/desc/true/1/1990/Action`,
   fetchEighties: `discovery/desc/true/2/1980/Action`,
-  // fetchDisney:
-  // fetchActor:
+  fetchDisney: `discovery/disney/esc/true/1/2020/disney`,
+  // fetchActor: `cast/ryanreynolds`,
   fetchComedy: `discovery/desc/true/1/2021/Comedy`,
   fetchAction: `discovery/desc/true/1/2021/Action`,
   fetchThriller: `discovery/desc/true/1/2021/Thriller`,
